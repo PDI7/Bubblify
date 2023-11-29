77d1a78e1e0948ef8c619f75476278adc9a203ae
PDI7 <patrik.messerli@gmx.ch>
2023-11-29 12:08:27 +0100

    feat: add firebase-auth-ktx dependence

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -68,6 +71,4 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
 }
