48d1e3a9062e3d0662c0352e2329aa738027a6bc
Célian CHAUSSON <celian.chausson@etu.univ-smb.fr>
2024-01-02 11:10:52 +0100

    Pre merge

--- app/src/main/java/com/example/bubblify/model/User.kt
+++ app/src/main/java/com/example/bubblify/model/User.kt
@@ -1,8 +1,8 @@
 package com.example.bubblify.model
 
 data class User(
-    val uuid: String,
     val username: String,
     val email: String,
-    var userGroups: List<UserGroup> = emptyList()
-)
+) {
+    constructor() : this("", "")
+}
